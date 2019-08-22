8cc31ded6b0060e7d1b0a532f14af4ab6431b3d1
Koyoko Wolf <54044163+koyokowolf@users.noreply.github.com>
2019-08-22 04:29:05 -0700

    Create Project: Ad Design.js

---
+++ Project: Ad Design.js
@@ -0,0 +1,31 @@
+fill(255, 0, 0);
+
+textSize(45);
+
+text("ACTION AVATARS!", 7, 34);
+
+fill(0,0,255);
+
+textSize(26);
+
+text("SO FUN! COLLECT THEM ALL!", 14, 60);
+
+fill(13, 171, 13);
+
+textSize(21);
+
+text("All avatars are available in action figures",3, 90);
+
+fill(102, 23, 120);
+
+textSize(25);
+
+text("Available in different sizes!!!", 50, 110);
+
+image(getImage("avatars/duskpin-ultimate"), 4, 138);
+
+image(getImage("avatars/leafers-ultimate"),258,120);
+
+image(getImage("avatars/aqualine-ultimate"), 180, 258);
+
+image(getImage("cute/Star"), 74, 194);
